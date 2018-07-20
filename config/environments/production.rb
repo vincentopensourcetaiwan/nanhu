51181709f557567367c92506ab6b4df7d5716470
vincent.open.source.taiwan <vincent.open.source.taiwan@gmail.com>
2018-07-20 03:56:31 -0400

    setup email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => "nanhu.me" }
+  config.action_mailer.default_url_options = { :host => "nanhu.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
