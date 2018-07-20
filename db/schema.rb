44a2bcf8aaedaed734846a7eb72f1352ec480418
vincent.open.source.taiwan <vincent.open.source.taiwan@gmail.com>
2018-07-20 02:57:40 -0400

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_065715) do
+ActiveRecord::Schema.define(version: 2018_07_20_065720) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_07_20_065715) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
