e7ac34c081f72dfc22f62ed15a2e048ee19b5257
vincent.open.source.taiwan <vincent.open.source.taiwan@gmail.com>
2018-10-23 10:04:19 -0400

    vincnet

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :registration_form1s
   resources :registration_form_nikis
-=======
   resources :registration_formtests
   resources :registration_form_test_abcs
->>>>>>> 33afe6f172d8d12a095dce44bb03c4138d33611d
   resources :registration_forms
   root to: 'visitors#index'
   devise_for :users
